19cf61e5e57c72b00b55ae902c2f3e9dc5fcaa98
Bruce chen <cqdeyouxiang@163.com>
2015-09-22 21:23:20 +0800

    Initial Commit

---
+++ QuarteringDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuarteringDemo
+//
+//  Created by Bruce on 15/9/22.
+//  Copyright © 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuarteringDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuarteringDemo
+//
+//  Created by Bruce on 15/9/22.
+//  Copyright © 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
